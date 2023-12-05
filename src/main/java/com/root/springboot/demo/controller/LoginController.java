71f7671a3cc237584d11f3f39089223f243bed9f
Parham Gandomkar <parham.gandomkar@gmail.com>
2023-12-05 15:05:12 +0100

    handling authentication form and restricting access

---
+++ src/main/java/com/root/springboot/demo/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.root.springboot.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+
+        // return "plain-login";
+        return "login";
+    }
+
+
+    // add request mapping for /access-denied
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+
+        return "access-denied";
+    }
+}
---
+++ src/main/java/com/root/springboot/demo/security/EmployeeSecurityApplication.java
@@ -0,0 +1,51 @@
+package com.root.springboot.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class EmployeeSecurityApplication {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                // Restrict Access Based on Roles
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/employees/delete").hasRole("MANAGER")
+                                .requestMatchers("/employees/showFormForAdd").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
+                )
+                .exceptionHandling(configurer ->
+                        configurer.accessDeniedPage("/access-denied")
+                );
+        ;
+
+
+
+        return http.build();
+    }
+
+
+}
